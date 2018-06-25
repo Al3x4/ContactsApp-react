d81fae2d707f71539bd3634025fc092d4a701a6e
Alex <alexandrapreda89@gmail.com>
2018-06-26 00:02:30 +0100

    add Show All button on search

--- src/ListContacts.jsx
+++ src/ListContacts.jsx
@@ -15,6 +15,8 @@ class ListContacts extends Component {
 		this.setState({ query : inputedQuery.trim() })
 	}
 
+	clearQuery = () => this.setState({query : ''})
+
 	render() {
 		//destructuring objects to write less
 		const {contacts, onDeleteContact} = this.props
@@ -40,6 +42,13 @@ class ListContacts extends Component {
 						onChange = {(event) => this.updateQuery(event.target.value)}
 					/>
 				</div>
+				{showingContacts.length !== contacts.length && (
+					<div className='showing-contacts'>
+						<span>Now showing {showingContacts.length} of {contacts.length}</span>
+						<button onClick={() => this.clearQuery()}>Show all</button>
+					</div>
+				)}
+
 				<ol className="contact-list">
 					{showingContacts.map((contact, index) => {
 						return(
