77d648bf687fba4dacf90c81dabe550a1e6c9d2b
Alex Preda <Alex.P@windomain.local>
2018-06-26 16:47:56 +0100

    add  external data source

--- src/App.js
+++ src/App.js
@@ -1,35 +1,27 @@
 import React, { Component } from 'react'
 import ListContacts from './ListContacts'
+import * as Contacts from './utils/ContactsAPI'
 
 class App extends Component {
+  
   state = {
-    contacts : [
-  {
-    "id": "ryan",
-    "name": "Ryan Florence",
-    "email": "ryan@reacttraining.com",
-    "avatarURL": "http://localhost:5001/ryan.jpg"
-  },
-  {
-    "id": "michael",
-    "name": "Michael Jackson",
-    "email": "michael@reacttraining.com",
-    "avatarURL": "http://localhost:5001/michael.jpg"
-  },
-  {
-    "id": "tyler",
-    "name": "Tyler McGinnis",
-    "email": "tyler@reacttraining.com",
-    "avatarURL": "http://localhost:5001/tyler.jpg"
+    contacts : []
   }
-]
+
+  componentDidMount() {
+    Contacts.getAll().then((contacts) => (
+      this.setState({contacts})
+      ))
   }
+
+
 
   removeContact = (clickedContact) => {
     this.setState((previousState) => ({
       contacts: previousState.contacts.filter((contactInState) => contactInState.id !== clickedContact.id)
     }))
   }
+
   render() {
     return (
       <div>
